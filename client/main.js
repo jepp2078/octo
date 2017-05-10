c7cf79ed4b89e0b89611d2d63230952de5092bb7
EGDK\xxzyx <xxzyx@eg.dk>
2017-05-10 20:17:55 +0200

    Added users. Navbar works

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import splash from '../imports/components/splash/splash';
+import '../imports/startup/accounts-config.js';
 
 angular.module('octo-app', [
   angularMeteor,
-  splash.name
+  splash.name,
+  'accounts.ui'
 ]);
--- imports/components/splash/splash.js
+++ imports/components/splash/splash.js
@@ -15,6 +15,53 @@ class SplashCtrl {
         $scope.aboutClass = ($scope.aboutSwitch == false ? 'glyphicon glyphicon-chevron-down' : 'glyphicon glyphicon-chevron-up');
       });
     }
+    $scope.navHeader = function() {
+      $('#about-collapse').collapse("hide");
+      $('#about-collapse1').collapse("show");
+      $('#about-collapse2').collapse("hide");
+      $('#signup-collapse').collapse("hide");
+      $('#header-collapse').collapse("show");
+    }
+    $scope.navSignup = function() {
+      $('#header-collapse').collapse("hide");
+      $('#about-collapse').collapse("hide");
+      $('#about-collapse1').collapse("show");
+      $('#about-collapse2').collapse("hide");
+      $('#signup-collapse').collapse("show");
+    }
+    $scope.navAbout = function() {
+      $scope.aboutClass = 'glyphicon glyphicon-chevron-down';
+      $scope.aboutSwitch = false;
+      $('#signup-collapse').collapse("hide");
+      $('#header-collapse').collapse("hide");
+      $('#about-collapse2').collapse("hide");
+      $('#about-collapse').collapse("show");
+      $('#about-collapse1').collapse("show");
+    }
+    $scope.submit = function () {
+        var data = {
+          profile: {name: $scope.name, phone: $scope.phone},
+          email: $scope.email,
+          password: $scope.password
+        }
+
+        return Meteor.call('createOctoUser', data, 'standard', function(err, userId) {
+          if (!err) {
+            $('#successModal').modal('show');
+            $('#name').val("");
+            $('#phone').val("");
+            $('#password').val("");
+            $('#email').val("");
+            setTimeout(function(){
+              $('#signup-collapse').collapse("hide");
+              $('#about-collapse').collapse("show")
+              $('#about-collapse1').collapse("show");
+            }, 500);
+          } else {
+            $('#errorModal').modal('show');
+          }
+        });
+    }
   }
 }
 
